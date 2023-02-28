706026ab93605a72b0adf354ab3313af28906876
Vaclav Dvorak <vaclav.dvorak@bootiq.io>
2023-02-28 14:03:19 +0100

    refactor: Rework 22/06 to testify framework

--- 2022/day06/main.go
+++ 2022/day06/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
-func main() {
-	for i := 0; i < len(input)-14; i++ {
+func solve(input string, lf int) int {
+	for i := 0; i < len(input)-lf; i++ {
 		found := map[byte]bool{}
 		nok := false
-		for look := 0; look < 14; look++ {
+		for look := 0; look < lf; look++ {
 			ch := input[i+look]
 			_, ok := found[ch]
 			if ok {
@@ -19,9 +23,35 @@ func main() {
 			}
 		}
 		if !nok {
-			fmt.Printf("%d -> %s\n", i+14, input[i:i+14])
-
-			break
+			fmt.Printf("%d -> %s\n", i+lf, input[i:i+lf])
+			return i + lf
 		}
 	}
+	return 0
+}
+
+func fs1(input io.Reader) int {
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+		return solve(line, 4)
+	}
+	return 0
+}
+
+func fs2(input io.Reader) int {
+	scanner := bufio.NewScanner(input)
+	for scanner.Scan() {
+		line := scanner.Text()
+		return solve(line, 14)
+	}
+	return 0
+}
+
+func main() {
+	f, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fs1(f)
 }
